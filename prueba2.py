45409d2b3aa56797094cce8307ee2082c4dca12f
erickalmazan <103240914+erickalmazan@users.noreply.github.com>
2024-05-01 17:51:56 -0600

    Update prueba2.py

--- prueba2.py
+++ prueba2.py
@@ -1,3 +1,4 @@
 print("Fichero dos v3")
 print("Cambio que le hizo la persona de login")
 print("Cambio de fichero para que aparazca el nuevo cambio en GitHub")
+print("Cambio de fichero para que aparezca el nuevo cambio en git")
